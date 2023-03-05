74a00028b00c91557c1a1a5e84f5e9afb1030c36
mcecula <mc23051988@gmail.com>
2023-03-05 18:55:01 +0100

    zadanie29

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 2
+let secondNumber = 2
+
+function multiplay (firstNumber, secondNumber){
+    let result = firstNumber * secondNumber;
+
+    return result;
+}
+console.log(multiplay (firstNumber, secondNumber))
